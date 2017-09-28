fbefe87f1286e511fa61d6855dbad45df1e6754c
DavidWork <steadypathapp@gmail.com>
2017-09-28 10:47:19 -0500

    Recreated the schema with graphql classes

--- tests/data/schema.ts
+++ tests/data/schema.ts
@@ -1,21 +1,32 @@
-const schema = `
-type Real implements Person {
+import { parseDGraph } from 'utils/parser'
+import { GraphQLDateTime } from 'graphql-iso-date'
+import {
+  GraphQLInterfaceType,
+  GraphQLObjectType,
+  GraphQLNonNull,
+  GraphQLString,
+  GraphQLList,
+  GraphQLSchema
+} from 'graphql'
+
+
+
+/**
+interface Person {
   name: String!
-  directed: [Movie]
-  acted: [Movie]
 }
+*/
+const personInterface = new GraphQLInterfaceType({
+  name: 'Person',
+  fields: () => ({
+    name: {
+      type: new GraphQLNonNull(GraphQLString)
+    }
+  })
+})
 
-type Fictional implements Person {
-  name: String!
-  playedBy: [Real]
-  in: [Movie]
-}
 
-type Genre {
-  genre: String!
-  movies: [Movie]
-}
-
+/**
 type Movie {
   title: String!
   starring: [Real]
@@ -24,8 +35,122 @@ type Movie {
   initial_release_date: Date
   characters: [Fictional]
 }
+*/
+const movieType = new GraphQLObjectType({
+  name: 'Movie',
+  fields: () => ({
+    title: {
+      type: new GraphQLNonNull(GraphQLString)
+    },
+    starring: {
+      type: new GraphQLList(realType)
+    },
+    genres: {
+      type: new GraphQLList(genreType)
+    },
+    directors: {
+      type: new GraphQLList(realType)
+    },
+    characters: {
+      type: new GraphQLList(fictionalType)
+    },
+    initial_release_date: {
+      type: GraphQLDateTime
+    },
+  }),
+})
 
-interface Person {
+
+/**
+type Real implements Person {
   name: String!
+  directed: [Movie]
+  acted: [Movie]
 }
-`
+*/
+const realType = new GraphQLObjectType({
+  name: 'Real',
+  interfaces: [personInterface],
+  fields: () => ({
+    name: {
+      type: new GraphQLNonNull(GraphQLString)
+    },
+    directed: {
+      type: new GraphQLList(movieType)
+    },
+    acted: {
+      type: new GraphQLList(movieType)
+    }
+  })
+})
+
+
+/**
+type Fictional implements Person {
+  name: String!
+  playedBy: [Real]
+  in: [Movie]
+}
+*/
+const fictionalType = new GraphQLObjectType({
+  name: 'Fictional',
+  interfaces: [personInterface],
+  fields: () => ({
+    name: {
+      type: new GraphQLNonNull(GraphQLString)
+    },
+    playedBy: {
+      type: new GraphQLList(realType)
+    },
+    in: {
+      type: new GraphQLList(movieType)
+    }
+  })
+})
+
+
+/**
+type Genre {
+  genre: String!
+  movies: [Movie]
+}
+*/
+const genreType = new GraphQLObjectType({
+  name: 'Genre',
+  fields: () => ({
+    genre: {
+      type: new GraphQLNonNull(GraphQLString)
+    },
+    movies: {
+      type: new GraphQLList(movieType)
+    }
+  })
+})
+
+
+/**
+*
+*/
+const queryType = new GraphQLObjectType({
+  name: 'Query',
+  fields: () => ({
+    movie: {
+      type: movieType,
+      resolve: (root, input, context, info) => parseDGraph(info)
+    }
+  })
+})
+
+
+/**
+*
+*/
+export const Schema = new GraphQLSchema({
+  query: queryType,
+  types: [
+    movieType,
+    realType,
+    fictionalType,
+    genreType
+  ]
+})
